2b40de93f06ec8eb90f5b5c59c82f8a3fa852073
Hieu Doan <hieu.doan@officeatwork.com>
2021-09-12 21:46:20 +0700

    Fix build failed

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -33,7 +33,7 @@ const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
 
       generateToken();
     }
-  }, [cart]);
+  }, [activeStep, history, cart]);
 
   const test = (data) => {
     setShippingData(data);
